b4a9d8e95a0b71bb42510c598b50ae075e9915b8
Hyeonwoo Lee <avacccc@gmail.com>
2022-04-07 22:38:36 +0900

    HW7

---
+++ HW7/HW7_client.py
@@ -0,0 +1,33 @@
+from socket import *
+import random
+import time
+
+port = 3333
+BUFF_SIZE = 1024
+
+sock = socket(AF_INET, SOCK_DGRAM)
+
+while True:
+    msg = input('-> ')
+    reTx = 0
+    while reTx <= 3:
+        resp = str(reTx) + ' ' + msg
+        sock.sendto(resp.encode(), ('localhost', port))
+        sock.settimeout(2) # 소켓의 timeout 설정. 해당 timeout 내 메시지
+                            # 수신을 못하면 timeout 예외 발생
+        try:
+            data, addr = sock.recvfrom(BUFF_SIZE)
+        except timeout:
+            reTx += 1
+            continue
+        else:
+            break
+    sock.settimeout(None) # 소켓의 블로킹 모드 timeout 설정
+    while True: # None인 경우, 무한정 블로킹됨
+        data, addr = sock.recvfrom(BUFF_SIZE)
+        if random.random() <= 0.5:
+            continue
+        else:
+            sock.sendto(b'ack', addr)
+            print('<-', data.decode())
+            break
---
+++ HW7/HW7_server.py
@@ -0,0 +1,34 @@
+from socket import *
+import random
+import time
+
+port = 3333
+BUFF_SIZE = 1024
+
+sock = socket(AF_INET, SOCK_DGRAM)
+sock.bind(('', port))
+
+while True:
+    sock.settimeout(None) # 소켓의 블로킹 모드 timeout 설정
+    while True: # None인 경우, 무한정 블로킹됨
+        data, addr = sock.recvfrom(BUFF_SIZE)
+        if random.random() <= 0.5:
+            continue
+        else:
+            sock.sendto(b'ack', addr)
+            print('<-', data.decode())
+            break
+    msg = input('-> ')
+    reTx = 0
+    while reTx <= 3:
+        resp = str(reTx) + ' ' + msg
+        sock.sendto(resp.encode(), addr)
+        sock.settimeout(2) # 소켓의 timeout 설정. 해당 timeout 내 메시지
+                            # 수신을 못하면 timeout 예외 발생
+        try:
+            data, addr = sock.recvfrom(BUFF_SIZE)
+        except timeout:
+            reTx += 1
+            continue
+        else:
+            break
