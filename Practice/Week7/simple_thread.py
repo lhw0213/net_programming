1949b34047bbc61f21cd5d1133923e97bf55f9a8
Hyeonwoo Lee <avacccc@gmail.com>
2022-04-18 21:56:06 +0900

    Practice

---
+++ Practice/Week7/simple_thread.py
@@ -0,0 +1,18 @@
+import threading
+
+def prtSquare(num):
+    print("Square: {}".format(num**2))
+
+def prtCube(num):
+    print("Cube: {}".format(num**3))
+
+t1 = threading.Thread(target=prtSquare, args=(10,))
+t2 = threading.Thread(target=prtCube, args=(10,))
+
+t1.start() # start thread 1
+t2.start() # start thread 2
+
+t1.join() # wait until thread 1 is completed
+
+t2.join() # wait until thread 2 is completed
+print('Done!')
